531ec3f125587caa7a1f2687abddea0beaaa4625
HJoan <66239936+Joan0018@users.noreply.github.com>
2021-07-10 17:36:27 +0800

    Add files via upload

---
+++ face-api.jsFaceDetection/script.js
@@ -0,0 +1,31 @@
+const video = document.getElementById('video')
+var start = new Date();   
+var dateStart = start.getHours() + ":" + start.getMinutes() + ":" + start.getSeconds() + ":" + start.getMilliseconds();
+console.log("Start Time = "+ dateStart)
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+]).then(startVideo)
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    var end = new Date(); 
+    var dateEnd = end.getHours() + ":" + end.getMinutes() + ":" + end.getSeconds() + ":" + end.getMilliseconds();
+    console.log("End Time = " + dateEnd)
+  }, 100)
+})
