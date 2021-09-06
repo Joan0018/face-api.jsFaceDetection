7fa98633262d20865c334c49ca34e4770d26c566
HJoan <66239936+Joan0018@users.noreply.github.com>
2021-09-07 06:49:19 +0800

    Update script.js

--- face-api.jsFaceDetection/script.js
+++ face-api.jsFaceDetection/script.js
@@ -1,4 +1,4 @@
-const video = document.getElementById('video')
+const video = document.getElementById('video');
 var start = new Date();   
 var dateStart = start.getHours() + ":" + start.getMinutes() + ":" + start.getSeconds() + ":" + start.getMilliseconds();
 console.log("Start Time = "+ dateStart)
@@ -7,25 +7,26 @@ Promise.all([
 ]).then(startVideo)
 
 function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+  // navigator.getUserMedia(
+  //   { video: {} },
+  //   stream => video.srcObject = vpath,
+  //   err => console.error(err)
+  // )
+  
+  video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      const resizedDetections = faceapi.resizeResults(detections, displaySize)
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+      faceapi.draw.drawDetections(canvas, resizedDetections)
+      var end = new Date(); 
+      var dateEnd = end.getHours() + ":" + end.getMinutes() + ":" + end.getSeconds() + ":" + end.getMilliseconds();
+      console.log("End Time = " + dateEnd)
+    }, 100)
+  })
 }
 
-video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
-  setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    var end = new Date(); 
-    var dateEnd = end.getHours() + ":" + end.getMinutes() + ":" + end.getSeconds() + ":" + end.getMilliseconds();
-    console.log("End Time = " + dateEnd)
-  }, 100)
-})
